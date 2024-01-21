d4e9127b292ca4599257dbb0067290ab7ffdddb2
Rosa Li <rosali@dhcp-33-186.cu-wireless-faculty.carleton.ca>
2024-01-21 15:47:39 -0500

    added download resume functionality

--- src/app/components/About.tsx
+++ src/app/components/About.tsx
@@ -7,8 +7,14 @@ import GithubIcon from "../../../public/github.svg";
 import LinkedinIcon from "../../../public/linkedin.svg";
 import DocumentIcon from "../../../public/document.svg";
 import MailIcon from "../../../public/mail.svg";
+import useDownloader from "react-use-downloader";
 
 const About = () => {
+  const { size, elapsed, percentage, download, cancel, error, isInProgress } =
+    useDownloader();
+
+  const fileUrl = "/RosaLiResume12212023.pdf"
+  const filename = "RosaLiResume12212023.pdf"
   return (
     <section className="lg:py-16">
       <div className="grid grid-cols-1 sm:grid-cols-12">
@@ -26,22 +32,21 @@ const About = () => {
               <Image src={MailIcon} alt="Mail Icon" />
             </Link>
             <Link href="https://github.com/rosali2003">
-            <Image src={GithubIcon} alt="Github Icon" />
+              <Image src={GithubIcon} alt="Github Icon" />
             </Link>
             <Link href="https://www.linkedin.com/in/rosaa-li/">
-            <Image src={LinkedinIcon} alt="LinkedIn Icon" />
+              <Image src={LinkedinIcon} alt="LinkedIn Icon" />
             </Link>
-            <Link href="../../../public/RosaLiResume09:06:2023.pdf">
-            <Image src={DocumentIcon} alt="Github Icon" />
+            <Link href={fileUrl}>
+              <Image src={DocumentIcon} alt="Github Icon" />
             </Link>
-            
           </div>
           <p className="text-[#ADB7BE] text-base sm:text-lg mb-6 lg:text-xl">
-            {"Hello! I'm Rosa, a 3rd year Computer Science student at"}{" "}
+            {"Hello! I'm Rosa, a production engineering intern at Shopify and"}{" "}
             <br></br>
-            {"Carleton University, Ottawa. I'm currently a Dev Degree"}{" "}
+            {"a 3rd year Computer Science student in the Dev Degree program at"}{" "}
             <br></br>{" "}
-            {"intern at Shopify, a frontend intern on the Shop Pay team"}
+            {" Carleton University, Canada."}
           </p>
 
           <div>
@@ -50,7 +55,9 @@ const About = () => {
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
-                Download CV
+                <button onClick={() => download(fileUrl, filename)}>
+                  Download Resume
+                </button>
               </span>
             </Link>
           </div>
