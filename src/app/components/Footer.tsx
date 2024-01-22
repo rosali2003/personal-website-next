e51956ef968a0c43e0dd088e8c318b1d303f0060
Rosa Li <rosali@rosas-mbp.home>
2024-01-22 08:56:57 -0500

    added projects and experiences

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -2,9 +2,9 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+    <footer className="footer z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white m-4">
       <div className="container p-12 flex justify-between">
-        <span>LOGO</span>
+        <span>RL</span>
         <p className="text-slate-600">All rights reserved.</p>
       </div>
     </footer>
--- src/app/components/Projects.tsx
+++ src/app/components/Projects.tsx
@@ -3,15 +3,19 @@ import React, { useState, useRef } from "react";
 import ProjectCard from "./ProjectCard";
 import ProjectTag from "./ProjectTag";
 import { motion, useInView } from "framer-motion";
+import { prototype } from "events";
 
-
+enum projectTypes {
+  projects = "Projects",
+  workExperience = "Work Experience"
+}
 const projectsData = [
   {
     id: 1,
     title: "Personal Website",
     description: "The website you're on now!",
     image: "/holder.png",
-    tag: ["Project"],
+    tag: [projectTypes.projects],
     gitUrl: "https://github.com/rosali2003/personal-website-next",
     previewUrl: "/",
   },
@@ -20,41 +24,77 @@ const projectsData = [
     title: "Agendify",
     description: "calendar-based task tracker",
     image: "/holder.png",
-    tag: ["Project"],
-    gitUrl: "/",
+    tag: [projectTypes.projects],
+    gitUrl: "https://github.com/rosali2003/agendify",
     previewUrl: "/",
   },
   {
     id: 3,
-    title: "Article",
-    description: "Project 3 description",
+    title: "Machine learning: Linear regression and logistic regression",
+    description: "Implemented a linear regression model with L2 and L1 norm to predict on the sonar dataset. Implemented logistic regression to predict on the auto mpg dataset.",
     image: "/holder.png",
-    tag: ["Article"],
-    gitUrl: "/",
-    previewUrl: "/",
-  },
-  {
-    id: 4,
-    title: "Returns",
-    description: "Contributed to making Return APIs",
-    image: "/holder.png",
-    tag: ["Work"],
-    gitUrl: "/",
+    tag: [projectTypes.projects],
+    gitUrl: "https://github.com/rosali2003/Machine-Learning/tree/main/A1",
     previewUrl: "/",
   },
   {
     id: 5,
-    title: "Passkeys on Shop Pay Checkout",
-    description: "Authentication and CRUD operations",
+    title: "Machine learning: Binary classification",
+    description: "Implemented a binary classification in adjoint form and SVM dual form. Utilized kernel functions to adjust the data for better model predictions.",
     image: "/holder.png",
-    tag: ["Work"],
+    tag: [projectTypes.projects],
+    gitUrl: "https://github.com/rosali2003/Machine-Learning/tree/main/A2",
+    previewUrl: "/",
+  },
+  {
+    id: 6,
+    title: "Machine learning: Multi-class classification",
+    description: "Implemented multi-class classification with multinomial deviance and principle component analysis to predict on the Fashion-MNIST dataset.",
+    image: "/holder.png",
+    tag: [projectTypes.projects],
+    gitUrl: "https://github.com/rosali2003/Machine-Learning/tree/main/A3",
+    previewUrl: "/",
+  },
+  {
+    id: 7,
+    title: "Machine learning: CNN",
+    description: "Implemented a CNN to predict on the MNIST dataset with 71% accuracy.",
+    image: "/holder.png",
+    tag: [projectTypes.projects],
+    gitUrl: "https://github.com/rosali2003/Machine-Learning/tree/main/A4",
+    previewUrl: "/",
+  },
+  {
+    id: 8,
+    title: "Back-end Developer Intern - Shopify",
+    description: "Developed APIs, GraphQL mutations and webhooks for the Returns team. (more info in resume :)",
+    image: "/shopify.png",
+    tag: [projectTypes.workExperience],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 9,
+    title: "Front-end Developer Intern - Shopify",
+    description: "Implemented passkeys into Guest Checkout flow. Led accessibility and translation effort and contributed to resiliency efforts. (more info in resume :)",
+    image: "/shopapp.png",
+    tag: [projectTypes.workExperience],
+    gitUrl: "/",
+    previewUrl: "/",
+  },
+  {
+    id: 10,
+    title: "Producting Engineering Intern - Shopify",
+    description: "Just started`. Stay tuned for upcoming adventures :)",
+    image: "/shopify.png",
+    tag: [projectTypes.workExperience],
     gitUrl: "/",
     previewUrl: "/",
   },
 ];
 
 const ProjectsSection = () => {
-  const [tag, setTag] = useState("All");
+  const [tag, setTag] = useState(projectTypes.projects);
   const ref = useRef(null);
   const isInView = useInView(ref, { once: true });
 
@@ -79,18 +119,18 @@ const ProjectsSection = () => {
       <div className="text-white flex flex-row justify-center items-center gap-2 py-6">
         <ProjectTag
           onClick={handleTagChange}
-          name="Project"
-          isSelected={tag === "Project"}
+          name={projectTypes.projects}
+          isSelected={tag === projectTypes.projects}
         />
-        <ProjectTag
+        {/* <ProjectTag
           onClick={handleTagChange}
           name="Article"
           isSelected={tag === "Article"}
-        />
+        /> */}
         <ProjectTag
           onClick={handleTagChange}
-          name="Work"
-          isSelected={tag === "Work"}
+          name={projectTypes.workExperience}
+          isSelected={tag === projectTypes.workExperience}
         />
       </div>
       <ul ref={ref} className="grid md:grid-cols-3 gap-8 md:gap-12">
